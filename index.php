befeb7d50c2484ffaa9c9ab95fc109f801a60ada
CarolinaReiss <cl.reiss@gmail.com>
2019-04-11 14:54:27 +0100

    Pushing Jessie's updates - post.php (to eventually replace product.php) and the new DB php_mvc. The sample DB Martina sent us is now renamed to php_mvc_skyQA

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
     <body>
         <?php
     require_once('connection.php');
-        
+        //the following will check what has been sent in the GET request viewable in the url
+    //if nothing is set, it will direct the user to pages/home.php
     if (isset($_GET['controller']) && isset($_GET['action'])) {
         $controller = $_GET['controller'];
         $action     = $_GET['action'];
