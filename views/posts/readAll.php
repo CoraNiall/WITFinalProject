fda8b0ffdeab5bc97cb8b911d9b508a678793cd9
CarolinaReiss <cl.reiss@gmail.com>
2019-04-14 12:37:20 +0100

    Updating for Update and Delete functions

--- views/posts/readAll.php
+++ views/posts/readAll.php
@@ -4,7 +4,7 @@
   <p>
     <?php echo $post->title; ?> &nbsp; &nbsp;
     <a href='?controller=post&action=read&id=<?php echo $post->id; ?>'>View post</a> &nbsp; &nbsp;
-    <a href='?controller=post&action=delete&id=<?php echo $post->title; ?>'>Delete post</a> &nbsp; &nbsp;
-    <a href='?controller=post&action=update&id=<?php echo $post->content; ?>'>Update post</a> &nbsp;
+    <a href='?controller=post&action=delete&id=<?php echo $post->id; ?>'>Delete post</a> &nbsp; &nbsp;
+    <a href='?controller=post&action=update&id=<?php echo $post->id; ?>'>Update post</a> &nbsp;
   </p>
 <?php } ?>
