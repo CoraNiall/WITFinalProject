284022d071e4ede6e0b29d6a7689dba2260c934f
LauraRowlands <49411916+LauraRowlands@users.noreply.github.com>
2019-04-17 21:13:03 +0100

    Updates to login

--- views/login/login.php
+++ views/login/login.php
@@ -1,3 +1,20 @@
 
-<?php 
-echo "Hello World. This is the login page.";
+<html>
+    <p>Fill in the following form to login:</p>
+<form action="" method="POST" class="w3-container" enctype="multipart/form-data">
+    
+    <h2>Login here!</h2>
+    <p>
+        <input class="w3-input" type="text" name="username" required autofocus>
+        <label>Username</label>
+    </p>
+        <p>
+        <input class="w3-input" type="text" name="password" required>
+        <label>Password</label>
+    </p>
+       
+  <p>
+    <input class="w3-btn w3-pink" type="submit" value="Login">
+  </p>
+</form>
+</html>
